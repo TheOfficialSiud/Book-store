df5c6e66a29d9e539e6b834e0ae2e4ca835032f0
Sudhanshu Tripathi <sudhaanshutripathi@gmail.com>
2023-12-03 14:01:35 +0530

    banner card implemented

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import BannerCard from '../home/BannerCard'
 
 function Banner() {
   return (
     <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
         <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40'>
         {/* {left side} */}
-        <div>
-            <h2 className='text-5xl font-bold leading-snuig text-black'>Buy and Sell Your Books</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quod est iusto! Voluptatibus eum molestias ipsum commodi sequi vero ipsam accusamus. Odio molestiae veritatis optio veniam necessitatibus eligendi repellendus dignissimos.</p>
+        <div className='md:w-1/2 space-y-8 h-full'>
+            <h2 className='text-5xl font-bold leading-snuig text-black'>
+                Buy and Sell Your Books
+                <span className='text-blue-700'> for the Best Prices</span></h2>
+            <p className='md:w-4/5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quod est iusto! Voluptatibus eum molestias ipsum commodi sequi vero ipsam accusamus. Odio molestiae veritatis optio veniam necessitatibus eligendi repellendus dignissimos.</p>
             <div>
                 <input type="search" name="search" id="search" placeholder='Search a book' className='py-2 px-2 rounded-s-sm outline-none'/>
                 <button className='bg-blue-700 text-white py-2 px-6 font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
@@ -15,7 +18,9 @@ function Banner() {
         </div>
 
          {/* {right side} */}
-         <div>right side</div>
+         <div>
+            <BannerCard></BannerCard>
+         </div>
 
         </div>
     </div>
