3cdafc4f71d211f9557f24fcc87e26a61b007ce0
Sudhanshu Tripathi <sudhaanshutripathi@gmail.com>
2023-12-03 13:17:05 +0530

    2:00:00 hr video complete

---
+++ mern-client/src/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'   
+const Home = () => {
+    return (
+        <div className=''>
+            <div className='h-screen'>
+                Home
+            </div>
+            <div className='h-screen bg-red-500'></div>
+        </div>
+    )
+}
+
+export default Home
